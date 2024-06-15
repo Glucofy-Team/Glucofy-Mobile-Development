d42dc501ec6ea4554c5828adbd90d800b4b806b7
Nicholas Yang <58143649+Lunatixcz@users.noreply.github.com>
2024-06-15 16:11:54 +0700

    Finished Food Detail Layout

--- app/src/main/java/com/dicoding2/glucofy/adapter/FoodAdapter.kt
+++ app/src/main/java/com/dicoding2/glucofy/adapter/FoodAdapter.kt
@@ -1,7 +1,30 @@
 package com.dicoding2.glucofy.adapter
 
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import com.dicoding2.glucofy.data.remote.response.FoodListItem
+import com.dicoding2.glucofy.databinding.ItemFoodBinding
+import com.dicoding2.glucofy.model.Food
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.NonDisposableHandle.parent
 
-class FoodAdapter : PagingDataAdapter<FoodListItem, FoodAdapter.FoodViewHolder>(DIFF_CALLBACK) {
+class FoodAdapter : PagingDataAdapter<FoodListItem, FoodViewHolder>(DIFF_CALLBACK) {
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        val food = getItem(position)
+
+        if (food!= null){
+            holder.bind(food)
+        }
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+        val binding = ItemFoodBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FoodViewHolder(binding)
+    }
 }
--- app/src/main/java/com/dicoding2/glucofy/adapter/FoodViewHolder.kt
+++ app/src/main/java/com/dicoding2/glucofy/adapter/FoodViewHolder.kt
@@ -1,5 +1,15 @@
 package com.dicoding2.glucofy.adapter
 
-class FoodViewHolder (val binding) {
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding2.glucofy.data.remote.response.FoodListItem
+import com.dicoding2.glucofy.databinding.ItemFoodBinding
 
+class FoodViewHolder (val binding: ItemFoodBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(food: FoodListItem) {
+        binding.tvFoodName.text = food.name
+        binding.tvCalories.text = food.caloriesKcal.toString()
+        binding.tvFats.text = food.fatsG.toString()
+        binding.tvProtein.text = food.proteinsG.toString()
+        binding.tvGlIndex.text = food.carbohydratesG.toString()
+    }
 }
